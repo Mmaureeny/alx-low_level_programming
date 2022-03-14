c502112c716fd2386cc6f8c1a228c84dd012d4fd
Mmaureeny <muthonimaureen4@outlook.com>
2022-03-14 01:17:00 -0700

    101-print-comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 
 /**
-* main - prints out all possible combos of 3 digits
+* main - program that prints all possible different combinations of
+* three digits
+* Numbers must be separated by ,followed by a space
+* The two digits must be different
+* 012, 120, 102, 021, 201, 210 are considered the same combination
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-int j;
-int k;
+int n1 = 48;
+int n2, n3;
+int com = 44;
 
-for (i = 48; i < 58; i++)
+while (n1 <= 57)
 {
-for (j = i + 1; j < 58; j++)
+n2 = n1 + 1;
+while (n2 <= 57)
 {
-for (k = j + 1; k < 58; k++)
+n3 = n2 + 1;
+while (n3 <= 57)
 {
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55 || j != 56 || k != 57)
+putchar(n1);
+putchar(n2);
+putchar(n3);
+if (n1 != 55 || n2 != 56 || n3 != 57)
 {
-putchar(',');
-putchar(' ');
+putchar(com);
+putchar(32);
 }
+n3 += 1;
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
 }
