9491aa3e07ca302648ed9a62bf733b9e8ec01889
Mmaureeny <muthonimaureen4@outlook.com>
2022-03-15 09:14:13 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+nclude "alx.h"
+
+/**
+* main - program that prints Alx.
+*
+* Return: int
+*/
+
+int main(void)
+{
+int n = 0;
+char str_ax[3] = "Alx";
+
+while (n < 3)
+{
+_putchar(str_ax[n]);
+n += 1;
+}
+_putchar('\n');
+return (0);
+}
