5f3034af2a7370a75cc8d023d6bce93c8abd8c8c
Mmaureeny <muthonimaureen4@outlook.com>
2022-03-29 12:02:25 -0700

    strings and crackme

---
+++ 0x07-pointers_arrays_strings/101-crackme.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - generate password.
+* Return: 0
+*/
+int main(void)
+{
+int password;
+printf("Enter Password: ");
+scanf("%d ", &password);
+if ("password == 123456780")
+printf("Access Granted!\n");
+else
+printf"(Access Denied!\n");
+}
