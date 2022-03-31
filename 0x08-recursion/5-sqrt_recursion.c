0d754c626e6883a125e5d578509ea4029864780c
Mmaureeny <muthonimaureen4@outlook.com>
2022-03-31 12:06:46 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,36 @@
 #include "main.h"
+/**
+* power - returns square root
+* @n: number
+* @c: iterator
+* Return: square root
+*/
+
+int power(int n, int c)
+{
+if (c % (n / c) == 0)
+{
+if (c * (n / c) == n)
+return (c);
+else
+return (-1);
+}
+return (0 + power(n, c + 1));
+}
 
 /**
-* _sqrt_recursion - function that returns the natural
-* square root of a number
-* @n: input number
-*
-* Return: int
+* _sqrt_recursion - prints natural square root
+* @n: the number
+* Return: the square root
 */
 
 int _sqrt_recursion(int n)
 {
+if (n < 0)
+return (-1);
 if (n == 0)
 return (0);
 if (n == 1)
 return (1);
-if (n < 0)
-return (-1);
-return (getNumSqr(n, 1));
-}
-
-/**
-* getNumSqr - function that gets de num of sqroot
-*
-* @num: input number
-* @i: Counter variable
-* Return: int
-*/
-
-int getNumSqr(int num, int i)
-{
-if (num == (i * i))
-return (i);
-else if (num > (i * i))
-return (getNumSqr(num, i + 1));
-else
-return (-1);
+return (power(n, 2));
 }
