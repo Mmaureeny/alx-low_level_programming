4518745587317433bc8faeeb68f71e9a382f5708
Mmaureeny <muthonimaureen4@outlook.com>
2022-04-07 22:22:33 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
-#include "mainn.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
 * _calloc - function that allocates memory for an array, using malloc
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,29 +1,25 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
-* array_range - function that creates an array of integers
-* @min: min value
-* @max: max value
+* array_range - creates an array of integers
+* @min: first integer number
+* @max: last integer number
 * Return: int
 */
-
 int *array_range(int min, int max)
 {
 int *arr;
-int size;
-int i = 0;
+int i;
 
 if (min > max)
-return (NULL);
-size = (max - min);
-arr = malloc(sizeof(*arr) * (size + 1));
+		return (NULL);
+arr = malloc(sizeof(*arr) * ((max - min) + 1));
+
 if (arr == NULL)
 return (NULL);
-while (i <= size)
-{
+
+for (i = 0; min <= max; min++, i++)
 arr[i] = min;
-i++;
-min++;
-}
+
 return (arr);
 }
