08b7080114c1449f1f56a02357583d518f0f4f79
Mmaureeny <muthonimaureen4@outlook.com>
2022-04-23 09:44:12 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,29 +12,23 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
+list_t *newNode;
 
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
+if (str == NULL)
 return (NULL);
-new_node->len = _strlen(str);
-new_node->str = strdup(str);
-new_node->next = *head;
-*head = new_node;
-return (new_node);
-}
-
-/**
-* _strlen - returns length of string
-* @s: character of string
-* Return: length of string
-*/
-
-int _strlen(const char *s)
+newNode = (list_t *)malloc(sizeof(list_t));
+if (newNode == NULL)
+return (NULL);
+newNode->str = strdup(str);
+if (newNode->str == NULL)
 {
-int i;
-
-while (s[i] != 0)
-i++;
-return (i);
+free(newNode);
+return (NULL);
 }
+newNode->len = strlen(str);
+newNode->next = *head;
+*head = newNode;
+
+return (newNode);
+}
+
